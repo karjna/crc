a40a69d33b245b3eaab12158a4733275c6c95430
karina <karina@creativengine.com>
2018-05-29 22:15:16 -0700

    Work controller, home and header styles

--- app/controllers/work_controller.rb
+++ app/controllers/work_controller.rb
@@ -1,11 +1,28 @@
 class WorkController < ApplicationController
     def index
-        @work = ButterCMS::Page.list('recent_work')      
+        @work = ButterCMS::Content.fetch([
+        :work_topic
+        ])
+        @work = @work.data      
     end
 
     def show
-        slug = params[:slug]
-    
+        slug = params[:slug]  
+           
+        @cases = []    
+        @work = ButterCMS::Content.fetch([:work_topic])
+
+        @work.data.work_topic.each { |item| 
+            if (item.url_slug == slug)
+                @work = item
+                @cases.push(item.cases)
+            end
+        }
+    end
+
+    def detail
+        slug = params[:slug] 
+        @parent = params[:topic]
         @work = ButterCMS::Page.get('recent_work', slug).data.fields
     end
 end
