4c176cab6643d2ddb819dab461c111b7273f3b08
karina <karina@creativengine.com>
2018-06-24 22:43:54 -0700

    Fix sprockets error

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,3 +13,5 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w( main.js )
+Rails.application.config.assets.precompile += %w( imagesloaded.pkgd.min.js )
+Rails.application.config.assets.precompile += %w( anime.min.js )
