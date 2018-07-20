1335f12a2e717174d92b88e910fcd6b04aa7199f
karina <karina@creativengine.com>
2018-07-20 06:26:36 -0700

    Stopgap forgetter

--- app/controllers/work_controller.rb
+++ app/controllers/work_controller.rb
@@ -9,14 +9,24 @@ class WorkController < ApplicationController
         @bodyClass = "topic"
         slug = params[:slug]  
            
+        @stopgap = []
         @cases = []    
         @work = ButterCMS::Content.fetch([:work_topic])
+        @recent = ButterCMS::Page.list('recent_work')
 
         @work.data.work_topic.each { |item| 
             if (item.url_slug == slug)
                 @work = item
                 @cases.push(item.cases)
             end
+        }
+
+        @cases[0].each { |item| 
+            @recent.each { |all| 
+            if (item.slug == all.data.slug)
+                @stopgap.push(all)
+            end
+         }
         }
 
         @cases = @cases[0]
