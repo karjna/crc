0549e5a929c4cc01344c218f246303cb9c89cd09
karina <karina@creativengine.com>
2018-08-28 17:20:31 -0700

    Add new fellows controller

--- app/controllers/team_controller.rb
+++ app/controllers/team_controller.rb
@@ -1,9 +1,13 @@
 class TeamController < ApplicationController
     def index
         @bodyClass = "team"
-        @sorted = ButterCMS::Content.fetch([
-        :team_members
-        ])
+        @sorted = ButterCMS::Content.fetch([:team_members])
+        @sorted = @sorted.data
+    end
+
+    def fellows
+        @bodyClass = "fellows"
+        @sorted = ButterCMS::Content.fetch([:fellows])
         @sorted = @sorted.data
     end
 
