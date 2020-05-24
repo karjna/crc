30b9bd446bbdc9a75620999eb0f856e0e0979341
Karina Carmona <karina.carmona@refinery29.com>
2020-05-24 19:00:32 -0400

    add images assets pipleine

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,13 @@ module Crc
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    Dir.glob("#{Rails.root}/app/assets/images/**/").each do |path|
+      config.assets.paths << path
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
 end
+
+
