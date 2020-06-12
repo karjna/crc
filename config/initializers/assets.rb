b20438601b0c811db85a7d9617983c98086e7a14
Karina Carmona <karina.carmona@refinery29.com>
2020-06-11 20:25:28 -0400

    add applicationcss to precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,6 +12,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( application.css )
 Rails.application.config.assets.precompile += %w( main.js )
 Rails.application.config.assets.precompile += %w( imagesloaded.pkgd.min.js )
 Rails.application.config.assets.precompile += %w( anime.min.js )
